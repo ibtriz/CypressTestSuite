efaeb7b347261d8c2a7e099deb78ca9520f97841
Beatriz <2205252@aluno.univesp.br>
2024-08-14 09:14:05 -0300

    Add navigation and interaction tests for the AdoPet application
    - Add test to visit the AdoPet homepage and click the "View pets available for adoption" button.
    - Implement test to verify the header buttons on the AdoPet homepage.
    - Add test to access the AdoPet login page.
    - Add test to access the AdoPet home page.
    - Implement test to visit the AdoPet home page and click the "Contact the responsible" button.

--- casos de teste/casos de teste.md
+++ casos de teste/casos de teste.md
@@ -12,3 +12,12 @@ O sistema processa as informações fornecidas.
 
 Regra de Negócio:
 E-mail e senha são campos obrigatórios para o cadastro.
+
+Exercícios
+Com o Cypress, escreva os códigos dos seguintes testes:
+
+Visite a página de principal do AdoPet e clique no botão ‘Ver pets disponíveis para adoção”;
+Visite a página de principal do AdoPet e teste os botões header;
+Visite a página de /login do Adopet;
+Visite a página de /home do Adopet;
+Visite a página de /home do AdoPet e clique no botão “Falar com o responsável”.
--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -9,3 +9,48 @@ describe('Teste de cadastro', () => {
     cy.contains('button', 'Cadastrar').click();
   })
 })
+
+//Visite a página de principal do AdoPet e clique no botão ‘Ver pets disponíveis para adoção”;
+describe('Ver pets disponíveis para adoção', () => {
+    it('botão: Ver pets disponíveis para adoção', () => {
+    cy.visit('https://adopet-frontend-cypress.vercel.app/');
+    cy.contains('a', 'Ver pets disponíveis para adoção').click();
+  })
+})
+
+//Visite a página de principal do AdoPet e teste os botões header;
+describe('Teste dos botões header', () => {
+    it('Teste dos botões header', () => {
+    cy.visit('https://adopet-frontend-cypress.vercel.app/');
+    cy.get('a.header__home').click();
+    cy.get('a.header__message').click();
+  })
+})
+
+//Visite a página de /login do Adopet;
+describe('Teste da página de login', () => {
+    it('Teste da página de login', () => {
+    cy.visit('https://adopet-frontend-cypress.vercel.app/login');
+    cy.get('a.header__home').click();
+    cy.contains('a', 'Fazer login').click();
+    cy.get('input[name="email"]').type('ana@email.com');
+    cy.get('input[name="password"]').type('Pass123456');
+    cy.contains('button', 'Entrar').click();
+  })
+})
+
+//Visite a página de /home do Adopet;
+describe('Teste da página de home', () => {
+    it('Teste da página de home', () => {
+    cy.visit('https://adopet-frontend-cypress.vercel.app/home');
+    cy.get('a.header__home').click();
+  })
+})
+//Visite a página de /home do AdoPet e clique no botão “Falar com o responsável”.
+describe('Falar com o responsável', () => {
+    it('botão: Falar com o responsável', () => {
+    cy.visit('https://adopet-frontend-cypress.vercel.app/home');
+    cy.contains('h4', 'Dunga') // Seleciona o card que contém o nome "Dunga"
+    cy.contains('a', 'Falar com responsável').click();
+  })
+})
